775c9e23e7ab862e5fb5058b3489503e26df374e
faranaan <aan21102006@gmail.com>
2026-02-11 22:39:06 +0700

    chore: initialize front project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-10 bg-white rounded-xl shadow-2xl text-center">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">
+          Setup Completed! 🚀
+        </h1>
+        <p className="text-gray-600">
+          Minggu 6: React + TypeScript + Tailwind v4 is Live.
+        </p>
+      </div>
+    </div>
+  )
+}
+export default App
